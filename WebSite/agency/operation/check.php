d93b5fcda8992f370ff5a34a7e3a8612d68d8731
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2023-08-03 16:01:24 +0200

    [MOD] now you can assume people and recreate the URSS

--- WebSite/agency/operation/check.php
+++ WebSite/agency/operation/check.php
@@ -5,13 +5,15 @@
     $ag_query = 'SELECT a.id FROM agenzia AS a WHERE a.nome = \''.$_SESSION["agency"].'\'';
     $ag_id = $conn -> query($ag_query) -> fetch_array();
     $ag = $ag_id["id"];             // agency id
+    $meta = $_POST["meta"];
 
-    foreach ($_POST as $x) {
+    foreach ($meta as $x) {
+        $date = $x["date"] == '' ? "NULL" : $x["date"];
         $insert_query = "INSERT INTO agenzia_utente (tipoContratto, scadenza, id_agenzia, id_utente)
-                VALUES ('{$x["contract"]}', '{$x["date"]}', '{$ag}', '{$x["userid"]}')";
+                VALUES ('{$x["contract"]}', {$date}, '{$ag}', '{$x["userid"]}')";
         $res = $conn -> query($insert_query);
     }
-    
+
     $agency_name = str_replace(' ', '+', $_SESSION["agency"]);
     header("location:../info_agency.php?agency={$agency_name}");
     
